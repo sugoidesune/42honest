ddcb76ac4a6cecc397de4a74531c11239cedee68
Timar <timar003@gmail.com>
2024-10-29 19:52:00 +0100

    Create get_next_line.c

---
+++ get_next_line.c
@@ -0,0 +1,5 @@
+Frequent Reasons to fail the project:
+
+| Fail Reason                      | Failcount |
+| -------------------------------- | --------- |
+| read must ALWAYS be used with *exactly* BUFFER_SIZE. Any read with different size is a fail. | 2 |
